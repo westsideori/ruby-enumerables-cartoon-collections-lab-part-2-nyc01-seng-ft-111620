20a2e4c1e29127b8604c9d97c82520d856995293
Ori Markowitz <ori.markowitz@gmail.com>
2020-10-24 13:27:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
-  array.map |element| do
+  array.map do |element| 
     element * element
   end
 end
