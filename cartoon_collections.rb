22db104e7511696af461ce2c1aaf76accae531de
Ori Markowitz <ori.markowitz@gmail.com>
2020-10-24 13:26:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
   array.map |element| do
-    
+    element * element
 end
 
 def summon_captain_planet(planeteer_calls)
