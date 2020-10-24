d8cd28dc1089845588bba9e2a1a94d950d65172b
Ori Markowitz <ori.markowitz@gmail.com>
2020-10-24 13:31:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  
+  planeteer_calls.any? do |element|
+    
 end
 
 def find_valid_calls(planeteer_calls)
