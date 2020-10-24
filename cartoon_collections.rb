cd6024ae94d601eac9cf9eb94fa02a7cab91bae8
Ori Markowitz <ori.markowitz@gmail.com>
2020-10-24 13:28:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,11 +9,13 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
+  
 end
 
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
+  
 end
 
 def find_valid_calls(planeteer_calls)
